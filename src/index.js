767cd7d95de53eaf082610aa2f68a3f4f80cce5e
Jesús Alejandro <jesusmolinarodrigue26@gmail.com>
2023-04-23 11:58:19 -0300

    feat: :sparkles: command handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 
 const client = new Telegraf(process.env.TOKEN);
+client.commands = new Map();
 
-client.start((ctx) => ctx.reply('Welcome'));
+client.on(message('text'), async (ctx) => {
+  if (!ctx.message.text.startsWith('/')) return;
+
+  const args = ctx.message.text.trim().slice(1).split(/ +/g);
+  const command = client.commands.get(args.shift().toLowerCase());
+
+  if (!command) return;
+
+  try {
+    await command.run(client, ctx, args);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const commandFiles = fs
+  .readdirSync(path.join(process.cwd(), 'src/commands'))
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const name = path.basename(file, path.extname(file));
+  const Module = await import(`./commands/${file}`);
+  const Class = new Module.default({});
+  client.commands.set(name, Class);
+}
 
 client.launch();
